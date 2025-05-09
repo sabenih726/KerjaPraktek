d64c78fa7b4f2f3c098d02db47dc4e7d270e9ca4
sabenih726 <sabenih726@gmail.com>
2025-05-09 10:05:10 +0700

    Update rename.py

--- utils/rename.py
+++ utils/rename.py
@@ -2,15 +2,12 @@ import os
 import re
 
 def clean_filename(value):
-    """Hilangkan karakter ilegal dari nama file."""
     value = re.sub(r"[\\/*?\"<>|]", "_", value)
     return value.strip().replace(" ", "_")
 
 def generate_new_filename(original_name, data, use_name=False, use_passport=False):
-    """Buat nama file baru berdasarkan opsi nama dan paspor."""
     base_name = os.path.splitext(original_name)[0]
 
-    # Ambil nama atau paspor dari data, per dokumen
     name = data.get("Name") or data.get("Nama TKA") or ""
     passport = data.get("Passport No") or data.get("Passport Number") or data.get("Nomor Paspor") or ""
 
@@ -20,9 +17,4 @@ def generate_new_filename(original_name, data, use_name=False, use_passport=Fals
     if use_passport and passport:
         parts.append(clean_filename(passport))
 
-    if parts:
-        new_name = "_".join(parts) + ".pdf"
-    else:
-        new_name = base_name + ".pdf"
-
-    return new_name
+    return "_".join(parts).strip("_") + ".pdf" if parts else base_name + ".pdf"
